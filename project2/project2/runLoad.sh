1f0a15a288cf7f498911954b29d8b6f8dd995b4a
Collin Yen <collinyen7@gmail.com>
2015-01-30 18:14:52 -0800

    Adding README and runload to repository

---
+++ project2/project2/runLoad.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Drop all the tables from the CS144 database
+mysql CS144 < drop.sql
+
+# Create the tables in the CS144 database
+mysql CS144 < create.sql
+
+# Compile the code
+ant
+
+# Run the script on all the data xml
+ant run-all
+
+sort -u Category.dat.tmp > Category.dat
+
+# Load all the data into the database
+mysql CS144 < load.sql
+
+# Remove the temporary data files
+rm *.dat
